cc08e147894a6bfa35b3381c9a7d4f4d571c7161
141naoto <naot.ishii141@gmail.com>
2018-09-29 11:15:21 +0900

    practice aws

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '20ba2d4e2f9ac1c2cddafa06b1bcb715f590740dcb135ae65cf68e087186579e63055552839bebc4ac670d3ccc3779e231a058c1b026c35d2045cb240ac7ef77'
